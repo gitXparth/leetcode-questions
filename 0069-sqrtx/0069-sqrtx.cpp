28de938fa5d0f6f263053b1d0d0f095499aed3e9
gitXparth <parthmishraa9918@gmail.com>
2026-08-18 10:26:04 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.7%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<2)
+            return x;
+        
+            int left = 0;
+            int right= x;
+            int ans = 0;
+            while(left <= right){
+                int mid = left +(right-left)/2;
+                if(mid <= x / mid){
+                    //mid*mid <x;
+                    ans = mid;
+                    left = mid+1;
+
+                }
+                else{
+                    right = mid-1;
+
+                }
+                    
+               
+            }
+        return ans;
+        
+    }
+};
