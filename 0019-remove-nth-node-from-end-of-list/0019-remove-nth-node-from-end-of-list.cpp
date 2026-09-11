0ec4136a5268878c1a583df37efc5b3cb7f2c69e
gitXparth <parthmishraa9918@gmail.com>
2026-09-11 10:31:09 +0530

    Time: 0 ms (100%), Space: 15.1 MB (25.94%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,22 +11,26 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // creating the dummy variable for easy deletion of the haed.
         ListNode* dummy =  new ListNode(0);
         dummy->next = head;
         ListNode* slow = dummy;
         ListNode* fast = dummy;
+        // this loop creates the gap of n;
         for(int i =0;i<n;i++){
             fast = fast->next;
         
         }
+        // this loop till the end of the linked list.
         while(fast->next != nullptr){
             slow= slow->next;
             fast = fast->next;
 
         }
+        // deletion happening here
         slow->next = slow->next->next;
         return dummy->next;
-        
+
         
     }
 };
